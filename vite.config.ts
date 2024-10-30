d32e275d627319de8783567aaae5028ae0417d59
21120321 <gamefun206@gmail.com>
2024-10-30 19:29:06 +0700

    inital project

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import {resolve} from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { register } from 'module';
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist'); 
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  root,
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        cart: resolve(root, 'cart','index.html'),
+        checkout: resolve(root, 'checkout','index.html'),
+        productDetails: resolve(root, 'product-details','index.html'),
+        shop: resolve(root, 'shop','index.html'),
+        login: resolve(root, 'login','index.html'),
+        register: resolve(root, 'register','index.html'),
+        about : resolve(root, 'about','index.html'),
+      },
+    },
+  },
+})
